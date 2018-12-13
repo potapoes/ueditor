8f3febfa3d2139b6f37a75ea86a370b8cfa3821d
zhouhongxia <zhouhongxia@sdas.org>
2018-12-13 11:30:39 +0800

    修改工具栏的样式

--- js/ueditor/ueditor.all.js
+++ js/ueditor/ueditor.all.js
@@ -25671,14 +25671,16 @@ UE.ui = baidu.editor.ui = {};
         },
         getHtmlTpl: function (){
             return '<div id="##" class="edui-box %%">' +
-                '<div id="##_state" stateful>' +
-                 '<div class="%%-wrap"><div id="##_body" unselectable="on" ' + (this.title ? 'title="' + this.title + '"' : '') +
-                 ' class="%%-body" onmousedown="return $$._onMouseDown(event, this);" onclick="return $$._onClick(event, this);">' +
-                  (this.showIcon ? '<div class="edui-box edui-icon"></div>' : '') +
-                  (this.showText ? '<div class="edui-box edui-label">' + this.label + '</div>' : '') +
-                 '</div>' +
-                '</div>' +
-                '</div></div>';
+                        '<div id="##_state" stateful>' +
+                            '<div class="%%-wrap"><div id="##_body" unselectable="on" ' + (this.title ? 'title="' + this.title + '"' : '') +
+                                ' class="%%-body" onmousedown="return $$._onMouseDown(event, this);" onclick="return $$._onClick(event, this);">' +
+                                (this.showIcon ? '<div class="edui-box edui-icon"></div>' : '') +
+                                (this.showText ? '<div class="edui-box edui-label">' + this.label + '</div>' : '') +
+                            '</div>' +
+                            (this.title ? this.title : '')+
+                            '</div>' +
+                        '</div>'+
+                    '</div>';
         },
         postRender: function (){
             this.Stateful_postRender();
